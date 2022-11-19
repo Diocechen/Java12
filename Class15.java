2b37b769a60b506d7d8bd15580020476cf5b4a2b
Diocechen <78402906+Diocechen@users.noreply.github.com>
2022-11-19 16:52:13 +0800

    Add files via upload

---
+++ Class15.java
@@ -0,0 +1,12 @@
+package Java12;
+
+public class Class15 {
+
+	public static void main(String[] args) {
+		Double a = 123.45;
+		String b = a.toString();
+		System.out.println(b);
+		System.out.println(b.length());
+	}
+
+}

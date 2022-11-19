97116e386f6de6691fc12460f70e5d82941c511b
Diocechen <78402906+Diocechen@users.noreply.github.com>
2022-11-19 17:08:22 +0800

    Add files via upload

---
+++ Class17.java
@@ -0,0 +1,11 @@
+package Java12;
+
+public class Class17 {
+
+	public static void main(String[] args) {
+		String a = "262904713";
+		Short b = Short.valueOf(a);
+		System.out.println(b*2);
+	}
+
+}
